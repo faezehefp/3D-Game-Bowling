02285ec8792f739ed93472e85ef7e31ccab67675
faezehefp <fantasticp.17@gmail.com>
2025-01-23 20:30:35 +0330

    Throw Ball, Collision Detection

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //برای نگهداری مرجع به کامپوننت ریجید بادی توپ
+    [SerializeField] Rigidbody BallRigidBody;
+
+    //تعیین توان و قدرت پرتاب توپ
+    [SerializeField] float Power;
+
+    void Start()
+    {
+        //به دست آوردن کامپوننت ریجیدبادی
+        BallRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //اگر کلید Enter فشرده شده بود
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            //نیرویی به سمت جلو به توپ اضافه می‌شود
+            BallRigidBody.AddForce(Vector3.forward * Power);
+        }
+    }
+}
